eeb1528dad138e120d795e23aa2b2a8543908f29
Ben Lawton <lawtonb16@gmail.com>
2022-08-21 02:33:51 -0500

    Added More Router Functionality

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -10,10 +10,11 @@ const CampsitesList = ({setCampsiteId}) => {
         <Row className='ms-auto'>
             {campsites.map(campsite => {
                 return(
-                <Col md='5' className='m-4' key={campsite.id} onClick={() => setCampsiteId(campsite.id)}>
+                <Col md='5' className='m-4' key={campsite.id}>
                     <CampsiteCard campsite={campsite} />
                 </Col>
-            )})}
+                );
+            })}
         </Row>
      );
 }
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,23 +1,12 @@
 import { Container, Row, Col } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectCampsiteById } from '../features/campsites/campsiteSlices';
-import CampsiteDetail from '../features/campsites/CampsiteDetail.js';
-import { useState } from 'react';
+
+
 
 const CampsitesDirectoryPage = () => {
-    const [campsiteId, setCampsiteId] = useState(0);
-    let selectedCampsite = selectCampsiteById(campsiteId);
-
     return ( 
         <Container>
-            <Row>
-                <Col sm='5' md='7'>
-                    <CampsitesList setCampsiteId={setCampsiteId} />
-                </Col>
-                <Col sm='7' md='5'>
-                    <CampsiteDetail campsite={selectedCampsite} />
-                </Col>
-            </Row>
+            <CampsitesList />
         </Container>
      );
 }
